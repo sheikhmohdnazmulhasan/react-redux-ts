c323df253a9a42c3aa56be43f9daf7ff6ade778a
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-09 17:13:14 +0600

    redux store created

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ function App() {
     <div className=" h-screen w-full flex justify-center items-center">
       <div className=" flex border border-purple-500 p-20 rounded-md bg-gray-100">
         <button className="px-3 py-2 bg-green-500 text-white font-semibold rounded-md">Increment</button>
+     
         <p className="text-3xl font-semibold px-10">0</p>
+
         <button className="px-3 py-2 bg-red-500 text-white font-semibold rounded-md">Decrement</button>
       </div>
     </div>
