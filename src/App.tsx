b456e6410a0c25c56251df5f3ad893790c77fe97
mdnazmul42726 <nazmulofficial@outlook.com>
2024-07-09 18:20:24 +0600

    Use Redux State and Actions in React Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useDispatch, useSelector } from "react-redux"
+import { decrement, increment } from "./redux/features/counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch();
 
   return (
     <div className=" h-screen w-full flex justify-center items-center">
       <div className=" flex border border-purple-500 p-20 rounded-md bg-gray-100">
-        <button className="px-3 py-2 bg-green-500 text-white font-semibold rounded-md">Increment</button>
-     
-        <p className="text-3xl font-semibold px-10">0</p>
 
-        <button className="px-3 py-2 bg-red-500 text-white font-semibold rounded-md">Decrement</button>
+        <button onClick={() => dispatch(increment())} className="px-3 py-2 bg-green-500 text-white font-semibold rounded-md">Increment</button>
+
+        <p className="text-3xl font-semibold px-10">{count}</p>
+
+        <button onClick={() => dispatch(decrement())} className="px-3 py-2 bg-red-500 text-white font-semibold rounded-md">Decrement</button>
       </div>
     </div>
   )
--- src/redux/features/counterSlice.ts
+++ src/redux/features/counterSlice.ts
@@ -1,12 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-interface TCounterState { count: number }
-
-const initialState: TCounterState = { count: 0 };
-
+// export interface TCounterState { count: number }
+const initialState = { count: 0 };
 
 const counterSlice = createSlice({
     name: 'counter',
     initialState,
-    reducers: {}
+    reducers: {
+
+        // increment the counter;
+        increment: (state) => {
+            state.count += 1;
+        },
+
+        // decrement the counter;
+        decrement: (state) => {
+            state.count -= 1;
+        },
+
+    }
 });
+
+export const { decrement, increment } = counterSlice.actions;
+export default counterSlice.reducer;
